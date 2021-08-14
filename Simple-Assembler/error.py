649897fd53afd2aacd565b449ecb0ed80bffb185
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-15 01:24:31 +0530

    Create error.py
    This file consists of all types of errors that can be there in the program and deals with them accordingly

---
+++ Simple-Assembler/error.py
@@ -0,0 +1,25 @@
+class SyntaxException(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)
+        pass
+    pass
+class ImmediateException(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)
+        pass
+    pass
+class IllegalHltInstruction(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)
+        pass
+    pass
+class IllegalVariable(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)
+        pass
+    pass
+class IllegalLabel(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)
+        pass
+    pass
