fa9ef7fea140cb68693d3e4f383d4bfaedbd85f7
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-24 01:52:56 +0530

    Update mov_func.py
    Some Bugs

--- SimpleSimulator/mov_func.py
+++ SimpleSimulator/mov_func.py
@@ -4,5 +4,6 @@ def mov_reg(register_file,reg_code1,reg_code2):
     return register_file
 def mov_imm(register_file,reg_code1,imm):
     val=imm
+    dict={'000':0,'001':1,'010':2,'011':3,'100':4,'101':5,'110':6,'111':7}
     register_file[dict[reg_code1]]='0'*8+val
     return register_file
