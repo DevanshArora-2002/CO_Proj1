451f5e68ff71ebe14b3a9f60e7f3437d1d4c70f3
Sharma-Devraj <88923263+Sharma-Devraj@users.noreply.github.com>
2021-08-23 23:00:01 +0530

    Update main.py
    Added input statements and fixed a bug relating to memory fetch instruction

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -3,14 +3,21 @@ from RegisterFile import RegisterRecord
 from Program_counter import program_counter
 from mem import Memory
 def main():
+    instructions = []
+    while True:
+        try:
+            instructions.append(input())
+        except EOFError:
+            break
     halted=False
     reg_obj = RegisterRecord()
     memory = Memory()
+    memory.initialize(instructions)
     PC = program_counter(0)
     execute = ExecutionEngine(memory, reg_obj.register_file, PC)
     cycle = 0
     while not halted:
-        instr=mem.fetch(PC.getval(),cycle)
+        instr=memory.fetch_using_PC(PC.getval(),cycle)
         halted,Next_PC=execute.execute(instr,cycle)
         PC.dump()
         reg_obj.dump()
