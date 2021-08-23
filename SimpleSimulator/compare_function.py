b49f9caf6dad2023caf2e8e143800f225acf9f40
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-24 03:12:32 +0530

    Update compare_function.py
    Some bugs

--- SimpleSimulator/compare_function.py
+++ SimpleSimulator/compare_function.py
@@ -1,19 +1,5 @@
-def binary_to_decimal(str):
-    num=0
-    for i in range(1,9):
-        num+=int(str[-i])*2**(i-1)
-    return num
-def decimal_to_binary(num):
-    binary = [0,0,0,0,0,0,0,0]
-    dividend = int(num)
-    quotient = -1
-    k=7
-    while(quotient!=0 and k>=0):
-        quotient = dividend//2
-        binary[k] = dividend%2
-        k=k-1
-        dividend = quotient
-    return "".join(map(str, binary))
+from binary_to_decimal import binary_to_decimal
+from decimal_to_binary import decimal_to_binary
 def cmp(register_file,reg_code1,reg_code2):
     dict = {'000': 0, '001': 1, '010': 2, '011': 3, '100': 4, '101': 5, '110': 6, '111': 7}
     str_val1 = register_file[dict[reg_code1]]
@@ -25,16 +11,11 @@ def cmp(register_file,reg_code1,reg_code2):
     and then sets the flag register accordingly
     """
     if(val1>val2):
-        str1=register_file[-1][0:-2]
-        str2=register_file[-1][-1]
-        register_file[-1]=str1+'1'+str2
+        register_file[-1]='0'*12+'0010'
         return register_file
     if(val1<val2):
-        str1=register_file[-1][0:-3]
-        str2=register_file[-1][-2:]
-        register_file[-1]=str1+'1'+str2
+        register_file[-1]='0'*12+'0100'
         return register_file
     else:
-        str1=register_file[-1][0:-1]
-        register_file=str1+'1'
+        register_file[-1]='0'*12+'0001'
         return register_file
