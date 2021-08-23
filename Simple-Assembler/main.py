90bacf2ae656591cde6c63ff7feb04129d31bca1
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-23 19:25:40 +0530

    Update main.py
    Line no was not printed

--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -4,21 +4,21 @@ from functionD_E import func_D,func_E
 from functionB_C import func_B_C
 from set_var_label import set_label_addr,set_var_addr
 import error
-def check_pnemonic(word,user_input,dict):
+def check_pnemonic(word,user_input,dict,line_no):
     if(word=='var'):
-        raise error.IllegalVariable("Variable declared in between")
+        raise error.IllegalVariable("Variable declared in between",line_no)
     lst_A=['add','sub','mul','xor','or','and']
     lst_B_C=['mov','div','ls','rs','cmp','not']
     lst_D=['ld','st']
     lst_E=['jmp','jlt','je','jgt']
     if(word in lst_A):
-        return func_A(user_input)
+        return func_A(user_input,line_no)
     elif(word in lst_B_C):
-        return func_B_C(user_input)
+        return func_B_C(user_input,line_no)
     elif(word in lst_D):
-        return func_D(user_input,dict)
+        return func_D(user_input,dict,line_no)
     elif(word in lst_E):
-        return func_E(user_input,dict)
+        return func_E(user_input,dict,line_no)
     else:
         return ""
 def main():
@@ -31,10 +31,8 @@ def main():
             break
     instructions=code.split('\n')
     if(len(instructions)>257):
-        raise error.SyntaxException("Instruction size exceeded")
+        raise error.SyntaxException("Instruction size exceeded",len(instructions)+1)
     instructions.pop(len(instructions)-1)
-    if('hlt' not in instructions[-1]):
-        raise error.IllegalHltInstruction("Hlt instruction missing")
     variables=[]
     i=0
     for i in range(0,len(instructions)):
@@ -52,23 +50,26 @@ def main():
         invid_instr=instructions[i]
         instr=invid_instr.split()
         if(i not in dict.values()):
-            val = check_pnemonic(instr[0], invid_instr, dict)
+            val = check_pnemonic(instr[0], invid_instr, dict,len(variables)+i+1)
             if(len(val)==0):
                 if(instr[0]=='hlt' and i==len(instructions)-1):
                     output+=func_F()
                 else:
-                    raise error.SyntaxException("Invalid Syntax")
+                    raise error.SyntaxException("Invalid Syntax",len(variables)+1+i)
             else:
                 output+=val+'\n'
             continue
         instr.pop(0)
         new_invid_instr=reduce(lambda a,b:a+" "+b,instr)
-        val=check_pnemonic(instr[0],new_invid_instr,dict)
+        if(i==len(instructions)-1):
+            if(new_invid_instr!='hlt'):
+                raise error.IllegalHltInstruction("Last Instruction is not hlt instruction",len(variables)+1+i)
+        val=check_pnemonic(instr[0],new_invid_instr,dict,len(variables)+i+1)
         if (len(val) == 0):
             if (instr[0] == 'hlt'):
                 output += func_F()
             else:
-                raise error.SyntaxException("Invalid Syntax")
+                raise error.SyntaxException("Invalid Syntax",dict,len(variables)+i+1)
         else:
             output += val + '\n'
     return output
