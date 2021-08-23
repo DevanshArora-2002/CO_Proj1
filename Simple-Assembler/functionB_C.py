38f657b002a774250ef211397db1ff5960179615
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-23 19:25:32 +0530

    Update functionB_C.py
    Line no was not printed

--- Simple-Assembler/functionB_C.py
+++ Simple-Assembler/functionB_C.py
@@ -1,12 +1,12 @@
 import error
 
-def decimal_to_binary(num):
+def decimal_to_binary(num,line_no):
     '''
     returns string containing 8 bit binary conversion of num
     num -> integer
     '''
     if(int(num)<0 or int(num)>255):
-        raise error.ImmediateException("Illegal immediate values")
+        raise error.ImmediateException("Illegal immediate values",line_no)
     binary = [0, 0, 0, 0, 0, 0, 0, 0]
     dividend = int(num)
     quotient = -1
@@ -33,31 +33,32 @@ def typeC(opcode, reg1, reg2):
     reg2 -> string
     '''
     return opcode + "00000" + reg1 + reg2
-def func_B_C(instruction):
+def func_B_C(instruction,line_no):
     '''
     returns the string encoding of instruction on the basis of whether it is type B or C
     instruction -> list
     '''
     instruction=instruction.split()
     if(len(instruction)!=3):
-        raise error.SyntaxException("Illegal use of syntax")
+        raise error.SyntaxException("Illegal use of syntax",line_no)
     reg_address = {"R0": "000", "R1": "001", "R2": "010", "R3": "011",
                    "R4": "100", "R5": "101", "R6": "110", "FLAGS": "111"}
     x=instruction[1]
     if(x not in reg_address):
-        raise error.SyntaxException("Invalid use of registers")
+        raise error.SyntaxException("Invalid use of registers",line_no)
     if (instruction[0] == "mov" and instruction[2][0] == "$"):  # type B
         opcode = "00010"
-        encoding = typeB(opcode, reg_address[instruction[1]], decimal_to_binary(instruction[2][1:]))
+        encoding = typeB(opcode, reg_address[instruction[1]], decimal_to_binary(instruction[2][1:],line_no))
     elif (instruction[0] == "mov" and instruction[2][0] != "$"):  # type C
         opcode = "00011"
+        
         if(instruction[2] not in reg_address):
-            raise error.SyntaxException("Invalid use of registers")
+            raise error.SyntaxException("Invalid use of registers",line_no)
         encoding = typeC(opcode, reg_address[instruction[1]], reg_address[instruction[2]])
     elif (instruction[0] == "div"):  # type C
         opcode = "00111"
         if (instruction[2] not in reg_address):
-            raise error.SyntaxException("Invalid use of registers")
+            raise error.SyntaxException("Invalid use of registers",line_no)
         encoding = typeC(opcode, reg_address[instruction[1]], reg_address[instruction[2]])
     elif (instruction[0] == "rs"):  # type B
         opcode = "01000"
@@ -68,11 +69,11 @@ def func_B_C(instruction):
     elif (instruction[0] == "not"):  # type C
         opcode = "01101"
         if (instruction[2] not in reg_address):
-            raise error.SyntaxException("Invalid use of registers")
+            raise error.SyntaxException("Invalid use of registers",line_no)
         encoding = typeC(opcode, reg_address[instruction[1]], reg_address[instruction[2]])
     elif (instruction[0] == "cmp"):  # type C
         opcode = "01110"
         if (instruction[2] not in reg_address):
-            raise error.SyntaxException("Invalid use of registers")
+            raise error.SyntaxException("Invalid use of registers",line_no)
         encoding = typeC(opcode, reg_address[instruction[1]], reg_address[instruction[2]])
     return encoding
