8456c1d7fc99414e6064b82b1e4b3e12009414c1
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-24 03:12:51 +0530

    Update jumping.py
    Some bugs

--- SimpleSimulator/jumping.py
+++ SimpleSimulator/jumping.py
@@ -7,9 +7,7 @@ def lt_jump(register_file,program_counter,mem_addr):
     flag_reg=register_file[-1]
     if(flag_reg[-3]=='1'):
         program_counter=binary_to_decimal(mem_addr)
-        str1=register_file[-1][0:-3]
-        str2=register_file[-1][-2:]
-        register_file=str1+'0'+str2
+        register_file[-1]='0'*16
     else:
         program_counter+=1
     return register_file,program_counter
@@ -17,9 +15,7 @@ def gt_jump(register_file,program_counter,mem_addr):
     flag_reg=register_file[-1]
     if(flag_reg[-2]=='1'):
         program_counter=binary_to_decimal(mem_addr)
-        str1=register_file[-1][0:-2]
-        str2=register_file[-1][-1]
-        register_file[-1]=str1+'0'+str2
+        register_file[-1]='0'*16
     else:
         program_counter+=1
     return register_file,program_counter
@@ -27,8 +23,7 @@ def eq_jump(register_file,program_counter,mem_addr):
     flag_reg=register_file[-1]
     if(flag_reg[-1]=='1'):
         program_counter=binary_to_decimal(mem_addr)
-        str1=register_file[-1][0:-1]
-        register_file[-1]=str1+'0'
+        register_file[-1]='0'*16
     else:
         program_counter+=1
     return register_file,program_counter
