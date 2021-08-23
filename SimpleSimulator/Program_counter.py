3b7e79a2cc9e27b03038f18254c9341284b7fd6d
DevanshArora-2002 <88950223+DevanshArora-2002@users.noreply.github.com>
2021-08-23 20:05:00 +0530

    Create Program_counter.py
    Take care of the program counter for the ISA

---
+++ SimpleSimulator/Program_counter.py
@@ -0,0 +1,31 @@
+from decimal_to_binary import decimal_to_binary
+from binary_to_decimal import binary_to_decimal
+class program_counter():
+    def __init__(self,counter_value):
+        """
+        The value of PC is saved in Integer format
+        :param counter_value:
+        """
+        self.pc=counter_value
+    def getval(self):
+        """
+        This returns PC in integer value format
+        :return:
+        """
+        return self.pc
+    def dump(self):
+        """
+        Returns the value in binary coded format
+        :return:
+        """
+        val=decimal_to_binary(self.pc)
+        return val
+    def update(self,counter_value):
+        """
+        Updates the value of PC
+        :param counter_value:
+        :return:
+        """
+        self.pc=counter_value
+        pass
+    pass
