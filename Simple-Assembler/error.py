8c3931ff03e62499027f2fccb9c3b09fc4709254
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-23 19:24:48 +0530

    Update error.py
    Line no was not printed along with error

--- Simple-Assembler/error.py
+++ Simple-Assembler/error.py
@@ -1,25 +1,25 @@
 class SyntaxException(Exception):
-    def __init__(self,msg):
-        super().__init__(msg)
+    def __init__(self,msg,line):
+        super().__init__(msg+" at line no-"+str(line))
         pass
     pass
 class ImmediateException(Exception):
-    def __init__(self,msg):
-        super().__init__(msg)
+    def __init__(self,msg,line):
+        super().__init__(msg+" at line no-"+str(line))
         pass
     pass
 class IllegalHltInstruction(Exception):
-    def __init__(self,msg):
-        super().__init__(msg)
+    def __init__(self,msg,line):
+        super().__init__(msg+" at line no-"+str(line))
         pass
     pass
 class IllegalVariable(Exception):
-    def __init__(self,msg):
-        super().__init__(msg)
+    def __init__(self,msg,line):
+        super().__init__(msg+" at line no-"+str(line))
         pass
     pass
 class IllegalLabel(Exception):
-    def __init__(self,msg):
-        super().__init__(msg)
+    def __init__(self,msg,line):
+        super().__init__(msg+" at line no-"+str(line))
         pass
     pass
