79ca68ed48ccccab3d4591cf2ca1b86bd139e617
Sharma-Devraj <88923263+Sharma-Devraj@users.noreply.github.com>
2021-08-25 19:58:37 +0530

    Update main.py
    Updates to incorporate the bonus question

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -16,13 +16,15 @@ def main():
     PC = program_counter(0)
     execute = ExecutionEngine(memory, PC)
     cycle = 0
+    accesses = {}
     while not halted:
         instr=memory.fetch_using_PC(PC.getval(),cycle)
-        halted,Next_PC,updated_reg_obj=execute.execute(instr,PC.getval(),reg_obj,cycle)
+        halted,Next_PC,updated_reg_obj,this_access=execute.execute(instr,PC.getval(),reg_obj,cycle)
         reg_obj=updated_reg_obj
         PC.dump()
         reg_obj.dump()
         PC.update(Next_PC)
+        accesses.update(this_access)
     memory.dump()
 if __name__ == "__main__":
     main()
