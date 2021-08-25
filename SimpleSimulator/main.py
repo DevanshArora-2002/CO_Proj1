07fdb50749f9d6573052cce7a0307133a73712ec
DevanshArora2002 <88903495+DevanshArora2002@users.noreply.github.com>
2021-08-26 03:44:13 +0530

    Update main.py
    Some bug in scatter function

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -3,6 +3,7 @@ from RegisterFile import RegisterRecord
 from Program_counter import program_counter
 import matplotlib.pyplot as plt
 from mem import Memory
+from binary_to_decimal import binary_to_decimal
 def main():
     instructions = []
     while True:
@@ -22,19 +23,22 @@ def main():
     while not halted:
         instr=memory.fetch_using_PC(PC.getval(),cycle)
         halted,Next_PC,updated_reg_obj,this_access=execute.execute(instr,PC.getval(),reg_obj,cycle)
+        if (len(this_access) > 0):
+            cycles.append(cycle)
+            cycles.append(cycle)
+            mem_accesses.append(PC.getval())
+            mem_accesses.append(binary_to_decimal(this_access[0]))
+        else:
+            cycles.append(cycle)
+            mem_accesses.append(PC.getval())
         reg_obj=updated_reg_obj
         PC.dump()
         reg_obj.dump()
         PC.update(Next_PC)
-        if(len(this_access)>0):
-            cycles.append(cycle)
-            cycles.append(cycle)
-            mem_accesses.append(instr)
-            mem_accesses.append(this_access)
-        else:
-            cycles.append(cycle)
-            mem_accesses.append(instr)
+        cycle+=1
     memory.dump()
+    print(cycles)
+    print(mem_accesses)
     plt.scatter(cycles, mem_accesses)
     plt.xlabel("Cycles")
     plt.ylabel("Memory Addresses")
